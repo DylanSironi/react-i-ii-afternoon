d48b99a4cdbbf294bf911af2268a2dbbdc82360b
DylanSironi <sironidylan@gmail.com>
2020-09-20 17:08:03 -0500

    start

---
+++ afternoon/src/Components/Comp1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+class Comp1 extends Component {
+    render(){
+        return(
+            <div>
+                wack
+            </div>
+        )
+    }
+}
+export default Comp1
---
+++ afternoon/src/Components/Comp2.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+class Comp2 extends Component {
+    render(){
+        return(
+            <div>
+                wack
+            </div>
+        )
+    }
+}
+export default Comp2
